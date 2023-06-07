3d0d11f10a1961a90feef40fe68b373e0af70bd6
aleksandrazb <ola.zbaska@wp.pl>
2023-06-07 14:54:14 +0200

    Added new add/delete directory activities

---
+++ app/src/main/java/com/txtlocker/DeleteDirectoryActivity.kt
@@ -0,0 +1,11 @@
+package com.txtlocker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeleteDirectoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete_directory)
+    }
+}
--- app/src/main/java/com/txtlocker/RemoveDirectoryActivity.java
+++
@@ -1,4 +0,0 @@
-package com.txtlocker;
-
-public class RemoveDirectoryActivity {
-}
